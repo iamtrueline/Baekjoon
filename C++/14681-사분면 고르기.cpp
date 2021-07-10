ac3f0aff69f4c3cbaa6d11e51d05ee5c750b49c0
iamtrueline <79238023+iamtrueline@users.noreply.github.com>
2021-07-10 18:35:16 +0900

    Create 14681-사분면 고르기.cpp

---
+++ C++/14681-사분면 고르기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(){
+    int x=0, y=0;
+    std::cin>>x>>y;
+    if(x>0&&y>0)
+        std::cout<<"1";
+    else if(x<0&&y>0)
+        std::cout<<"2";
+    else if(x<0&&y<0)
+        std::cout<<"3";
+    else if(x>0&&y<0)
+        std::cout<<"4";
+    return 0;
+}
