4ea3ff30e1dc98e58c2f791bca6ff16d23c146ad
Jinseon KANG <iamtrueline@gmail.com>
2021-09-08 21:20:31 +0900

    Create 13236-Collatz Conjecture.c

---
+++ C/13236-Collatz Conjecture.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d ", n);
+    
+    while(n != 1){
+        if(n%2 == 0){
+            n = n/2;
+            printf("%d ", n);
+        }
+        else{
+            n = n*3+1;
+            printf("%d ", n);
+        }
+    }
+    return 0;
+}
