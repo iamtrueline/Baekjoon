9b047e5ba0716df00171a407ca5ab57a4d025cdd
Jinseon KANG <iamtrueline@gmail.com>
2021-09-21 15:36:43 +0900

    Create 4673-셀프 넘버.cpp

---
+++ C++/4673-셀프 넘버.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int d[10001];
+
+int dn(int i) {
+    int res = i;
+    if (i >= 10000) {res += i / 10000; i %= 10000; }
+    if (i >= 1000) { res += i / 1000; i %= 1000; }
+    if (i >= 100) { res += i / 100; i %= 100; }
+    if (i >= 10) { res += i / 10; i %= 10; }
+    return res += i;
+}
+
+int main() {
+    for (int i = 1; i <= 10000; i++) {
+        d[dn(i)] = 1;
+    }
+    for (int i = 1; i <= 10000; i++) {
+        if (!d[i]){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
